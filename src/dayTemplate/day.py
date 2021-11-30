e81720df4d33ff8b81085bb30e1dac7987cf2180
Per Grapatin <per.grapatin@gmail.com>
2021-11-30 16:54:52 +0100

    first commit

---
+++ src/dayTemplate/day.py
@@ -0,0 +1,33 @@
+"""
+Template code
+"""#import cmath for complex number operations
+#from abc import abstractproperty
+#import cmath
+#import Path for file operations
+from pathlib import Path
+
+PROGBLEM_INPUT_TXT = Path("/Users/pergrapatin/Source/AOC2015/src/"\
+    +"dayXX/input.txt").read_text()
+
+EXAMPLE_INPUT1 = """"""
+EXAMPLE_RESULT1 = 1514
+
+def string_worker(input_string):
+    """Helper string worker function
+    """
+    a_steps = input_string.split("\n")
+    return a_steps
+
+def problem_a(input_string, expected_result):
+    """Problem A solved function
+    """
+    solution = input_string
+
+    if solution == expected_result:
+        print("Correct solution found:", solution)
+    else:
+        print("Incorrect solution, we got:", solution, "expected:", expected_result)
+
+problem_a(EXAMPLE_INPUT1, EXAMPLE_RESULT1)
+problem_a(PROGBLEM_INPUT_TXT, 0)
+print("\n")
